d6c53761a32d42f542c4b635e2d5c23a46d11b55
Tom Grand <thomas.a.grand@gmail.com>
2018-01-04 22:26:50 +0000

    test deck can be given cards, and passes

--- game/deck.rb
+++ game/deck.rb
@@ -2,7 +2,15 @@ require_relative "cards"
 
 class Deck
 
+     def initialize() 
+       @card_store = []
+     end
+
 	def take_card 
 		Card.new
+    end
+
+    def give_cards(pile)
+      @card_store += pile
     end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -6,9 +6,9 @@ describe Deck do
   it {is_expected.to respond_to(:take_card)}
 
   it "take card should return a card" do 
-
   	expect(Deck.new.take_card.is_a?(Card)).to eq true 
-  	
   end
+
+  it {is_expected.to respond_to(:give_cards)}
 
 end 
